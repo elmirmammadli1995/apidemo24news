9a50fd48c0d895f6985b78362b7e032db823b527
elmirmammadli1995 <149464393+elmirmammadli1995@users.noreply.github.com>
2025-04-04 08:30:10 +0000

    ModelsPy

--- api/models.py
+++ api/models.py
@@ -19,8 +19,139 @@ news2 = News(
 )   "https://picsum.photos/800?random=2"
 
 news3 = News(
-    
+    "M3gan 2.0 gives the doll some upgrades in new trailer",
+    "Though Blumhouse’s first M3gan feature was a near-perfect blend of techno-horror and ridiculous comedy, the sequel looks like it’s going to blow its predecessor out of the water.",
+    "https://picsum.photos/800?random=3"
 )
+
+news4 = News(
+    "Microsoft’s miniature Windows 365 Link PC is available to buy now",
+    "Microsoft’s business-oriented “Link” mini-desktop PC, which connects directly to the company’s Windows 365 cloud service, is now available to buy for $349.99 in the US and in several other countries. Windows 365 Link, which was announced last November, is a device that is more easily manageable by IT departments than a typical computer while also reducing the needs of hands on support.",
+    "https://picsum.photos/800?random=4"
+
+)
+
+news5 = News(
+    "Sonos says it’s ‘assessing potential implications’ of Trump tariffs for customers",
+    "Even with all the self-induced turmoil that Sonos has been through over the last year, if you’ve been eyeing any of the company’s products, it might be wise to buy sooner than later. In an email, spokesperson Erin Pategas tells me that Sonos is “closely monitoring developments related to the proposed tariffs and actively assessing potential implications for our business, customers, and supply chain.",
+    "https://picsum.photos/800?random=5"
+)
+
+ news6 = News(
+    "Here are the best iPad deals you can get",
+    "While the best iPad deals usually land during major sale events like Black Friday and Amazon Prime Day, many great iPad deals are attainable outside those times. The day-to-day discounts may come and go like changing winds, but there’s often something to be saved, particularly on the more affordable iPads.",
+    "https://picsum.photos/800?random=6"
+ )
+
+news7 = News(
+    "Verizon now offers a three-year price lock — but there’s a catch",
+    "Verizon is announcing a new price lock policy today, and the timing is probably no coincidence. The company is extending a three-year price guarantee on certain plans, both for new and existing customers. The announcement comes a day after President Trump unveiled sweeping tariffs, and Verizon says it’s hoping to give customers more “predictability” in the current economic environment.",
+    "https://picsum.photos/800?random=7"
+)
+
+news8 = News(
+    "Amazon is ready to launch its Starlink competitor",
+    "The first batch of 27 Project Kuiper space internet satellites are scheduled to launch next week. Amazon has secured 80 such launch missions that will each deliver dozens of satellites into low earth orbit (LEO) to create a constellation capable of competing with Elon Musk’s Starlink juggernaut.",
+    "https://picsum.photos/800?random=8"
+)
+
+news9 = News(
+    "Amazon can now buy products from other websites for you",
+    "Amazon is testing a new “Buy for Me” button that will let you purchase products from third-party websites without leaving the e-commerce giant’s mobile app. The feature is powered by agentic AI, allowing the company to purchase items on your behalf.",
+    "https://picsum.photos/800?random=9"
+)
+
+news10 = News(
+    "Genki has a magnetic battery for the Switch 2 and a better Joy-Con charging dock",
+    "With every new portable console comes a mountain of new third-party accessories. We’ve already seen Hori’s Piranha Plant alternative to the Switch 2’s USB-C camera, but Genki has announced a larger lineup of accessories designed to help protect the portable console while you’re playing away from home, plus some clever ways to keep it and its Joy-Con controllers charged.",
+    "https://picsum.photos/800?random=10"
+)
+
+news11 = News(
+    "TikTok is shutting down its Instagram-like Notes app",
+    ""TikTok is shutting down Notes, its photos-sharing app that rivaled Instagram. In a notification to users, the TikTok Notes team says the app will stop working starting May 8 th, “all related features will no longer be available.”,
+    "https://picsum.photos/800?random=11"
+)
+
+news12 = News(
+    "The Kindle’s new Recaps feature will catch you up on a book series",
+    "Amazon is comparing a new feature for the Kindle to the “Previously on…” segments that TV shows frequently use. But the Kindle’s Recaps feature is instead focused on book series and provides a “quick refresher on storylines and character arcs” before readers start the next book, Amazon says.",
+    "https://picsum.photos/800?random=12"
+)
+
+news13 = News(
+    "Capitalism vs. the bird flu",
+    "On today’s episode, we’re talking about bird flu, but in a pretty Decoder way. Lauren Leffer, who recently wrote a piece for The Verge about bird flu and how it’s becoming a forever war, joined me on the show, and we talked about the systems, structure, and culture that might control bird flu — and those that might make it worse.",
+    "https://picsum.photos/800?random=13"
+)
+
+news14 = News(
+    "How the Tesla brand turned so toxic",
+    "Today we’re talking about the protests against Tesla, which have been branded on social media under the hashtag Tesla Takedown. The protests are, of course, a reaction to Elon Musk, who has managed to install himself as basically a not-so-shadow president who is tearing the federal government apart, leaving confusion and destruction in his wake.",
+    "https://picsum.photos/800?random=14"
+)
+
+news15 = News(
+    "AV1 is supposed to make streaming better, so why isn’t everyone using it?",
+    "When you jump into a video on YouTube or Netflix, a lot happens very quickly behind the scenes. Video data is rapidly downloaded to your device, which then has to unpack and normalize that information into a smooth, hiccup-free stream. The process of encoding and decoding video data has changed greatly over the years, with H.264 (AVC) and its successor H.265 (HEVC) remaining two of the most widely used codecs for streaming.",
+    "https://picsum.photos/800?random=15"
+)
+
+news16 = News(
+    "Micro USB is the bane of my existence",
+    "Last year, my best friend bought me an HP Sprocket portable photo printer. This, she said, would take my journaling game to the next level. After a short demo and dozens of journaling TikToks, I was stoked. And then I unboxed the damn thing.",
+    "https://picsum.photos/800?random=16"
+)
+
+news17 =News(
+    "Why the watt is the most important spec in battery-powered devices",
+    "Every time I see “mAh” on a consumer product, I cringe. Megahertz are starting to bruise, too. That’s because the most critical components of portable gadgets — batteries and processors — should really be measured in watts: it’s the one common spec that gives you a real idea of what your device can do.",
+    "https://picsum.photos/800?random=17"
+)
+
+news18 = News(
+    "Superman’s latest trailer introduces his team of robot assistants",
+    "Though Superman is clearly going to get the living daylights beat out of him at some point in James Gunn’s upcoming DC Studios feature, the movie’s latest trailer puts a spotlight on how the Man of Steel gets put back together again.",
+    "https://picsum.photos/800?random=18"
+)
+
+news19 = News(
+    "Sonos says it’s ‘assessing potential implications’ of Trump tariffs for customers",
+    "Even with all the self-induced turmoil that Sonos has been through over the last year, if you’ve been eyeing any of the company’s products, it might be wise to buy sooner than later. In an email, spokesperson Erin Pategas tells me that Sonos is “closely monitoring developments related to the proposed tariffs and actively assessing potential implications for our business, customers, and supply chain.",
+    "https://picsum.photos/800?random=19"
+)
+
+news20 = News(
+    "Windows 11 tests taskbar icons that scale up and down like on a Mac",
+    "Microsoft is testing out resizable taskbar icons in Windows 11 that can shrink as you open more apps or stay at a smaller size for good. The new options are included in the latest Insider Preview Beta.",
+    "https://picsum.photos/800?random=20"
+)
+
+news21 = News(
+    "Amazon can now buy products from other websites for you",
+    "Amazon is testing a new “Buy for Me” button that will let you purchase products from third-party websites without leaving the e-commerce giant’s mobile app. The feature is powered by agentic AI, allowing the company to purchase items on your behalf.",
+    "https://picsum.photos/800?random=21"
+)
+
+news22 = News(
+    "We found 50 deals you can still get from Amazon’s spring sale for $100 or less",
+    "From the Kindle Colorsoft and Samsung’s new Galaxy S25 to the top-of-the-line Roborock S8 MaxV Ultra, Amazon’s Big Spring Sale has brought steep discounts on some of our favorite gadgets.",
+    "https://picsum.photos/800?random=22"
+)
+
+news23 = News(
+    "Steve Jackson Games says tariffs are a ‘seismic shift’ for board games",
+    "The CEO of Steve Jackson Games, which makes board games and card games, says that the 54 percent tariff on goods imported from China that will go into effect on April 5th is a “seismic shift” for the board game industry and that “prices are going up.”",
+    "https://picsum.photos/800?random=23"
+)
+
+news24 = News(
+    "PSA pauses card grading submissions from outside the US",
+    "The card grading service PSA will no longer take direct submissions from outside the US in response to the Trump administration’s new far-reaching tariffs. In an update on Wednesday, PSA says it’s “reluctantly taking these measures to protect our international customers from significant tariff expenses.”",
+    "https://picsum.photos/800?random=24"
+)
+
+
 
 
 new_list = [news1, news2, news3, news4, news5, news6, news7, news8, news9, news10, news11, news12, news13, news14, news15, news16,  news17,  news18,  news19,  news20,  news21, news22,  news23,  news24] 
