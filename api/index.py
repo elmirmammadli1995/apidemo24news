1d5921629804840f201be6a7af648ddab42771fd
elmirmammadli1995 <149464393+elmirmammadli1995@users.noreply.github.com>
2025-04-06 09:04:13 +0000

    origins

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from .routes import get_news
 app = Flask(__name__)
 
 CORS(app, resource = {r"/api/news": {
-"origins": [""],
+"origins": ["https://elmirmammadli1995.github.io"],
 "methods": ["GET"]
 }})
 
