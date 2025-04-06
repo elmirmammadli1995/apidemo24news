961ba6c6476c1233603431b037ffd8922c5493ae
elmirmammadli1995 <149464393+elmirmammadli1995@users.noreply.github.com>
2025-04-06 07:37:53 +0000

    ApiDemo

---
+++ api/models.py
@@ -0,0 +1,155 @@
+class News:
+
+    def __init__(self, tit, des, img):
+
+        self.tit = tit
+        self.des = des
+        self.img = img
+
+
+news1 = News(
+    "How the Nintendo Switch 2 stacks up to the Steam Deck",
+    "The biggest threat to the Steam Deck in 2025 won’t be the arrival of other Windows-based handheld gaming PCs. Instead, it’ll likely be the Nintendo Switch 2, which arrives on June 5 for $449.99 — smack dab between the cost of the $399 Steam Deck LCD and the $549 Steam Deck OLED.",
+    "https://picsum.photos/800?random=1"
+)
+
+news2 = News(
+    "This ’90s-esque social media site only works for three hours a day",
+    "For the past few weeks, every day at 7:38 PM ET, I get an email titled “seven39 is open again.” From 7:39PM, I have exactly three hours to check out an experimental new social media site before it completely shuts down.",
+    "https://picsum.photos/800?random=2"
+)
+
+news3 = News(
+    "Honda is sending its hydrogen tech to space",
+    "Honda is looking to the stars for its next hydrogen breakthrough. The automaker is teaming up with space tech companies Sierra Space and Tec-Masters to test its high-differential pressure water electrolysis system on the International Space Station.",
+    "https://picsum.photos/800?random=3"
+)
+
+news4 = News(
+    "Microsoft has created an AI-generated version of Quake",
+    "Microsoft unveiled its Xbox AI era earlier this year with a new Muse AI model that can generate gameplay. While it looked like Muse was still an early Microsoft Research project, the Xbox maker is now allowing Copilot users to try out Muse through an AI-generated version of Quake II.",
+    "https://picsum.photos/800?random=4"
+)
+
+news5 = News(
+    "Microsoft CEOs interrupted by another employee protestor: ‘shame on all of you’",
+    "In the second major disruption of today’s 50th anniversary event at Microsoft’s headquarters, another employee stood up and began yelling at Satya Nadella, Steve Ballmer, and Bill Gates — the company’s current and past CEOs — in protest of Microsoft’s dealings with the government of Israel.",
+    "https://picsum.photos/800?random=5"
+)
+
+news6 = News(
+    "Meta releases two Llama 4 AI models",
+    "Meta has announced Llama 4, its newest collection of AI models that now power the Meta AI assistant on the web and in WhatsApp, Messenger, and Instagram. The two new models, also available to download from Meta or Hugging Face, are Llama 4 Scout — a small model capable of “fitting in a single Nvidia H100 GPU” — and Llama 4 Maverick, which is more akin to GPT-4o and Gemini 2.0 Flash.",
+    "https://picsum.photos/800?random=6"
+)
+
+news7 = News(
+    "Tron: Ares blends the real world with the digital in its first trailer",
+    "Disney just released the first trailer for Tron: Ares, the long-planned Tron: Legacy sequel. The minute-and-a-half trailer doesn’t say much about the story but shows plenty of the movie’s visuals, which look dark, moody, and filled with the series’ signature light trails.",
+    "https://picsum.photos/800?random=7"
+)
+
+news8 = News(
+    "Jaguar Land Rover pauses US shipments over Trump tariffs",
+    "Jaguar Land Rover (JLR) says it’s delaying shipments to the US this month while it works out how it will deal with the wide-ranging tariffs President Donald Trump announced this week, according to The Guardian.",
+    "https://picsum.photos/800?random=8"
+)
+
+news9 = News(
+    "Starlink competition is ramping up in Ukraine",
+    "French satellite network company Eutelsat has been providing Ukraine with much-needed internet access for almost a year with the help of the German government, Reuters reports.",
+    "https://picsum.photos/800?random=9"
+)
+
+news10 = News(
+    "Ring’s founder is back at Amazon",
+    "Ring founder Jamie Siminoff returned to Amazon this week, coming back to the company just about two years after he left, Bloomberg reports. He’s now a vice president at the company, and he will be heading up the Ring, Blink, Amazon Key, and Sidewalk teams.",
+    "https://picsum.photos/800?random=10"
+)
+
+news11 = News(
+    "Meta’s fact-checking program in the US officially ends soon",
+    "Meta’s US fact-checking program will come to an end by Monday afternoon. In a post on X, Meta’s global policy head Joel Kaplan said there will be “no new fact checks and no fact checkers” across Facebook, Instagram, and Threads by that time as the company replaces it with Community Notes.",
+    "https://picsum.photos/800?random=11"
+)
+
+news12 = News(
+    "Rural fiber internet expansion is at risk as Trump administration holds funds",
+    "Fiber internet providers are worried they won’t see the funds promised under a Biden-era initiative that would bring reliable internet service to rural areas.",
+    "https://picsum.photos/800?random=12"
+)
+
+news13 = News(
+    "More than 1,200 rallies worldwide protest Trump and Musk",
+    "People are gathering in cities all over the United States and globally to protest an “illegal, billionaire power grab” by President Donald Trump and Elon Musk. They’re being put on by over 150 different organizations, including civil rights groups, labor unions, and LGBTQ+ advocates, and span more than 1,200 locations.",
+    "https://picsum.photos/800?random=13"
+)
+
+news14 = News(
+    "Meta releases two Llama 4 AI models",
+    "Meta has announced Llama 4, its newest collection of AI models that now power the Meta AI assistant on the web and in WhatsApp, Messenger, and Instagram. The two new models, also available to download from Meta or Hugging Face, are Llama 4 Scout — a small model capable of “fitting in a single Nvidia H100 GPU” — and Llama 4 Maverick, which is more akin to GPT-4o and Gemini 2.0 Flash.",
+    "https://picsum.photos/800?random=14"
+)
+
+news15 = News(
+    "Microsoft brings Copilot Vision to Windows and mobile for AI help in the real world",
+    "Microsoft is bringing its Copilot Vision feature to Windows and mobile, moving it beyond the web. Copilot Vision was a big part of Microsoft’s Copilot redesign last year, but so far, it has been limited to Edge webpages to help guide you through what you’re seeing.",
+    "https://picsum.photos/800?random=15"
+)
+
+news16 = News(
+    "Microsoft Copilot can now use the web on your behalf",
+    "Microsoft is making changes to Copilot that will allow the artificial intelligence assistant to complete online tasks for you. Using “simple chat prompts,” these “Actions” can be performed in the background while you work on other tasks, according to Microsoft, and can do things like book restaurant reservations, event tickets, and purchase items to ship to your friends.",
+    "https://picsum.photos/800?random=16"
+)
+
+news17 = News(
+    "An immersive version of The Wizard of Oz is coming to the Las Vegas Sphere",
+    "Sphere Entertainment has announced two new immersive experiences coming to its 516-foot wide spherical venue in Las Vegas. An enhanced version of The Wizard of Oz will premiere on the Sphere’s 160,000 square foot screen on August 28th, 2025, while a new film by Academy Award-winning directors Elizabeth Chai Vasarhelyi and Jimmy Chin (Free Solo) featuring climber Alex Honnold and other elite athletes is scheduled for 2026.",
+    "https://picsum.photos/800?random=17"
+)
+
+news18 = News(
+    "DOGE staffers are listed in the FCC directory",
+    "Elon Musk’s Department of Government Efficiency (DOGE) has infiltrated the Federal Communications Commission (FCC), an agency that has a say over resources Musk needs or could benefit from for some of his private sector business, The Verge has learned.",
+    "https://picsum.photos/800?random=18"
+)
+
+news19 = News(
+    "Sam Altman's AI-generated cricket jersey image gets Indians talking",
+    "India is a cricket-crazy nation, and it seems the AI chatbot ChatGPT hasn't missed that fact. So, when its founder Sam Altman fed it the prompt: "Sam Altman as a cricket player in anime style", the bot seems to have immediately generated an image of Altman wielding a bat in a bright blue India jersey.",
+    "https://picsum.photos/800?random=19"
+)
+
+news20 = News(
+    "India's rivers are home to 6,000 dolphins - but they are in trouble",
+    "India's longest and most holy river, the Ganges, is home to thousands of dolphins. But their survival is under threat. But these aren't like the ones found in oceans. They don't leap out of the water in spectacular arcs; surface for long intervals or swim in an upright position.",
+    "https://picsum.photos/800?random=20"
+)
+
+news21 = News(
+    "FromSoftware’s Miyazaki assures fans the studio will still make single-player games",
+    "In a multi-part interview with Nintendo about newly-announced Switch 2 exclusive The Duskbloods, director Hidetaka Miyazaki tried to dispel worries that developer FromSoftware is shifting focus away from single-player games.",
+    "https://picsum.photos/800?random=21"
+)
+
+news22 = News(
+    "Nvidia confirms the Nintendo Switch 2 has DLSS and real-time ray tracing",
+    "Nintendo’s Switch 2 tech specs yesterday vaguely said that the console is powered by a “custom processor made by Nvidia,” but in a new blog post, Nvidia has shared a little more detail about how it’s powering the device.",
+    "https://picsum.photos/800?random=22"
+)
+
+news23 = News(
+    "ZeniMax union votes to authorize a strike",
+    "Members of ZeniMax Workers United, a union of over 300 quality assurance workers in Texas and Maryland, have voted to authorize a strike.",
+    "https://picsum.photos/800?random=23"
+)
+
+news24 = News(
+    "The best wireless earbuds to buy right now",
+    "It’s hard to buy a bad pair of wireless earbuds these days, and with constant discounts and deals wherever you look, now is as good of a time as any to splurge on the pair you’ve been eyeing.",
+    "https://picsum.photos/800?random=24"
+)
+
+
+new_list = [news1, news2, news3, news4, news5, news6, news7, news8, news9, news10, news11, news12, news13, news14, news15, news16, news17, news18, news19, news20, news21, news22, news23, news24]
